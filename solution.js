1a072cba8ce967ac72145681a8f95074eda0623e
Hizkiel-Mesfin <mesfinezekiel@gmail.com>
2022-08-24 15:48:39 +0200

    Highly thotical

---
+++ solution.js
@@ -0,0 +1,43 @@
+const now = new Date();
+const test = new Date();
+
+console.log(!now);
+console.log(test);
+console.log(new Date());
+console.log(Date);
+
+//#1
+// The now variable is an instance created from the Date constructor function
+
+//#2
+// The now is an object
+
+//#3
+// The Date is a builtin class
+
+//#4
+// Date is a function
+
+//#5
+// You get the current Date and time
+
+//#5
+// A function would be returned when conosle.logging "Date"
+
+//#7
+//You get the same thing thing as you would get from console.logging now ot or test
+
+//#8
+//Yes now is Truthy
+
+//#9
+// False
+
+//#10
+// False
+
+//#11
+// False
+
+//#12
+// False becasue new date can't have another one of it's own. It one of a kind
